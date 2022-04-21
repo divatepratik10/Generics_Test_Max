a180afce1cab828253aa758329e07954c8059d77
divatepratik10 <pratikrdivate3210@gmail.com>
2022-04-22 00:01:05 +0530

    [Pratik] added : Generics Array of different data types

---
+++ Generic_Demo/PrintArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Demo
+{
+    internal class PrintArray
+    {
+        public static void PrintArray_Elements(int[] arr)
+        {
+            foreach(int element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public static void PrintArray_Elements(double[] arr)
+        {
+            foreach(double element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public static void PrintArray_ele(params int[] arr)
+        {
+            foreach (int element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public static void Print_Array(char[] arr)
+        {
+            foreach(char element in arr)
+            {
+                Console.WriteLine((char)element);
+            }
+        }
+
+    }
+}
---
+++ Generic_Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Method Programme.");
+            Console.WriteLine("Enter Size of Array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int [] intarr = new int[size];
+            Console.WriteLine("Enter Array Elements : ");
+            for(int i = 0;i<size;i++)
+            {
+                intarr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            double[] doublearr = { 11.22, 33.21, 43.42 };
+            char[] chararr = { 'a', 'b', 'c', 'd', 'e', };
+            PrintArray.PrintArray_Elements(intarr);
+            Console.WriteLine("\n");
+            PrintArray.PrintArray_Elements(doublearr);
+            Console.WriteLine("\n");
+            PrintArray.PrintArray_ele(10);
+            Console.WriteLine("\n");
+            PrintArray.PrintArray_ele(20,30);
+            Console.WriteLine("\n");
+            PrintArray.PrintArray_ele(40,50,60);
+            Console.WriteLine("\n");
+            PrintArray.Print_Array(chararr);
+            Console.ReadLine();
+        }
+    }
+}
